1beec576d7ffcc951fc98d77b2672d19c2d88425
Viet Anh Vo <vietanhvo@pm.me>
2024-06-06 10:53:03 +0700

    Fix next plugin

--- dist/next-config/index.js
+++ dist/next-config/index.js
@@ -2,13 +2,16 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+const eslintrc_1 = require("@eslint/eslintrc");
+const js_1 = __importDefault(require("@eslint/js"));
 const react_config_1 = __importDefault(require("../react-config"));
-const eslint_plugin_next_1 = __importDefault(require("@next/eslint-plugin-next"));
+const compat = new eslintrc_1.FlatCompat({
+    recommendedConfig: js_1.default.configs.recommended,
+    allConfig: js_1.default.configs.all,
+});
 const configs = [
     ...react_config_1.default,
-    {
-        ...eslint_plugin_next_1.default.configs["core-web-vitals"],
-    },
+    ...compat.extends("plugin:@next/next/core-web-vitals"),
 ];
 module.exports = configs;
 //# sourceMappingURL=index.js.map
--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,12 +1,16 @@
-import reactPlugin from "../react-config";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import { TSESLint } from "@typescript-eslint/utils";
-import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "../react-config";
+
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 const configs: TSESLint.FlatConfig.ConfigArray = [
   ...reactPlugin,
-  {
-    ...nextPlugin.configs["core-web-vitals"],
-  },
+  ...compat.extends("plugin:@next/next/core-web-vitals"),
 ];
 
 export = configs;

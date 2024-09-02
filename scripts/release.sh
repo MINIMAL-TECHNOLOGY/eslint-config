4fa8bc4cbffe0c5049af9c8bc564a319d518e04a
Phat Nguyen <tanphat199@gmail.com>
2024-09-02 14:34:14 +0700

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -32,4 +32,17 @@ echo "Publishing $module"
 cd packages/$module
 pnpm publish --no-git-check
 
+PACKAGE_PATH="packages/$module/package.json"
+VERSION=$(jq -r .version $PACKAGE_PATH)
+
+git fetch
+git checkout develop
+git add .
+git commit -m "Module: $module | Version: $VERSION | Release latest src"
+git push origin develop
+git checkout main
+
+git merge develop
+git push origin main
+
 echo "DONE | Releasing module $module"

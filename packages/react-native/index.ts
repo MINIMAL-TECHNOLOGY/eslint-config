8d1288af9eb368a24df6f96ea5bca8a75bed0892
Phat Nguyen <tanphat199@gmail.com>
2024-09-15 13:59:57 +0700

    normalize react-native

--- packages/react-native/index.ts
+++ packages/react-native/index.ts
@@ -1,16 +1,43 @@
 import reactPlugin from '@minimaltech/eslint-react';
+import tsEslint from "typescript-eslint";
 
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 
-const compat = new FlatCompat({
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const normalizeExpoConfigs = () => {
+  const compat = new FlatCompat({
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+  });
 
+  const rs: ReturnType<typeof tsEslint.config>= []
+  const expoConfs = compat.extends("eslint-config-expo");
+  for (const conf of expoConfs) {
+    if (!conf?.plugins?.['@typescript-eslint']) {
+      rs.push(conf);
+      continue;
+    }
+
+    delete conf.plugins['@typescript-eslint'];
+    if (!Object.keys(conf.plugins).length){
+      delete conf.plugins;
+    }
+
+    rs.push(conf);
+  }
+
+  return rs;
+}
+
+// ------------------------------------------------------------
 const configs = [
-  ...compat.extends("eslint-config-expo"),
+  ...normalizeExpoConfigs(),
   ...reactPlugin,
+  {
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
   {
     ignores: ['scripts/**/*', 'assets/**/*'],
   },
